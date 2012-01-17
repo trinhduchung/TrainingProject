407681e34cfeabd3de9d1ea3ce42498a68c15e82
hung <trinhduchung266@gmail.com>
2012-01-17 17:23:33 +0700

    fix error

---
+++ iphone/TextVideoApp/TextVideoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextVideoApp
+//
+//  Created by Cuong Tran on 1/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iphone/TextVideoApp/TextVideoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TextVideoApp
+//
+//  Created by Cuong Tran on 1/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
